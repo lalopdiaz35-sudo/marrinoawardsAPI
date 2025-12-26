c1ec0f1797a1b5de1efec772b732d0d22d2a2a0a
eduardo <lalopdiaz35@gmail.com>
2025-12-25 18:46:51 -0800

    fix5: variables añadidas

--- config/db.js
+++ config/db.js
@@ -2,15 +2,17 @@
 const mongoose = require('mongoose');
 
 const connectDB = async () => {
+    if (mongoose.connection.readyState >= 1) return;
+
     try {
-        
-        const conn = await mongoose.connect(process.env.MONGO_URI);
+        const conn = await mongoose.connect(process.env.MONGO_URI, {
+            serverSelectionTimeoutMS: 5000
+        });
 
         console.log(`📡 MongoDB Conectado: ${conn.connection.host}`);
     } catch (error) {
         console.error(`🚨 Error de Conexión a MongoDB: ${error.message}`);
-        
-        process.exit(1);
+        throw error; 
     }
 };
 
